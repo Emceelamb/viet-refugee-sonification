479b4ec1db8d861319293427a144dc0a138c875a
Emceelamb <Emceelamb@users.noreply.github.com>
2022-07-08 00:59:57 +0000

    deploy: 9fe504b5128cea543701fd50860b348703af8b7f

--- _next/static/g9x_DgnL_fNNcxBxRRpS4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/185-7720788cb23b3812.js","static/chunks/pages/index-d8dde0e81f2fb9b9.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
